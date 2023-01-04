e0ad9728250e1782c7fca4dce37244305eed235d
sivlauvset <64396205+sivlauvset@users.noreply.github.com>
2023-01-04 14:53:32 +0100

    Update glodap2_csv2mat.m
    Code changed to read GLODAPv2 files which include text strings with doi and expocode

--- glodap2_csv2mat.m
+++ glodap2_csv2mat.m
@@ -4,8 +4,10 @@ function glodap2_csv2mat(cruise_folder,datafile_name,expocode_file_name)
 % .mat files
 %
 % Input: cruise_folder - This is the folder where your data files are located
-%        datafile_name - This is the name of the .csv file that contains
-%                        all the reference data
+%        datafile_name - This is the name of the file (without file extension) that contains
+%                        all the reference data (same filename for both the
+%                        .mat and .csv files). NB! This routine loads the .mat
+%                        file!
 %        expocode_file_name - This is the name of the .csv file that
 %                             contains the expocodes associated with the cruise numbers in the
 %                             data file
@@ -16,26 +18,21 @@ function glodap2_csv2mat(cruise_folder,datafile_name,expocode_file_name)
 %
 %
 % Siv Lauvset 2015-03-10
-% last modified 2015-07-28
+% last modified 2022-10-03
 
 fclose('all');
 
 outputfilename='2ndQC_ReferenceData';
 
-% load data
-fname=cat(2,[cruise_folder filesep datafile_name]);
-G2data=csvread(fname,1,0); % skip first line which is headers
-sz=size(G2data);
-
-% load headers
+% load headers data
+fname=cat(2,[cruise_folder filesep datafile_name '.csv']);
 fid=fopen(fname,'r');
-G2headers=textscan(fid,'%s',sz(2),'delimiter',',');
-G2headers=G2headers{:};
-
-G2header=cell(size(G2headers));
+G2headers=textscan(fid,'%s',109,'delimiter',',');
+G2headers=G2headers{:};sz=size(G2headers);
 
 % make sure headernames are correct (prefix G2 on all)
-for i=1:sz(2)
+G2header=cell(size(G2headers));
+for i=1:sz(1)
     H=char(G2headers{i});
     strcheck=strcmp(H(1:2),'G2');
     if strcheck==0
@@ -48,62 +45,71 @@ for i=1:sz(2)
     clear strcheck newname
 end
 
-% name all vars
-for i=1:sz(2)
-    var=[G2header{i,:}, ' = G2data(:,i);'];
-    eval(var)
-    clear var
+% load data
+G2data=load([cruise_folder filesep datafile_name '.mat']);
+
+% define reference variables, i.e. those used in 2QC
+ref_vars={'G2cruise';'G2station';'G2region';'G2cast';'G2year';'G2month';'G2day';'G2hour';'G2minute';'G2latitude';'G2longitude';'G2bottomdepth';'G2maxsampdepth';'G2bottle';'G2pressure';'G2depth';'G2temperature';'G2theta';'G2salinity';'G2salinityf';'G2salinityqc';...
+    'G2sigma0';'G2sigma1';'G2sigma2';'G2sigma3';'G2sigma4';'G2gamma';'G2oxygen';'G2oxygenf';'G2oxygenqc';'G2aou';'G2aouf';'G2nitrate';'G2nitratef';'G2nitrateqc';'G2silicate';'G2silicatef';'G2silicateqc';'G2phosphate';'G2phosphatef';'G2phosphateqc';...
+    'G2tco2';'G2tco2f';'G2tco2qc';'G2talk';'G2talkf';'G2talkqc';'G2fco2';'G2fco2f';'G2fco2temp';'G2phts25p0';'G2phts25p0f';'G2phtsinsitutp';'G2phtsinsitutpf';'G2phtsqc';'G2cfc11';'G2pcfc11';'G2cfc11f';'G2cfc11qc';'G2cfc12';'G2pcfc12';'G2cfc12f';'G2cfc12qc';...
+    'G2cfc113';'G2pcfc113';'G2cfc113f';'G2cfc113qc';'G2ccl4';'G2pccl4';'G2ccl4f';'G2ccl4qc';'G2sf6';'G2psf6';'G2sf6f';'G2sf6qc'};
+
+for i=1:size(ref_vars,1)
+var=cat(2,['ref_data(:,i)=double(G2data.',ref_vars{i,:},');']);
+eval(var)
 end
 
 % use only data with QC-flag=1 in the reference data
-for i=1:sz(2)
-    H=char(G2header{i});
+for i=1:size(ref_vars,1)
+    H=char(ref_vars{i});
     strcheck=strcmp(H(end-1:end),'qc');
-    if strcheck==1 & i<53
+    if strcheck==1 & i<55
         %--------Modified by Ilaria Stendardo----------------
         %var=cat(2,['G2data(G2header{i,:}==0,i-2)=nan;']);
-        var=cat(2,['G2data(',G2header{i,:},'==0,i-2)=nan;']);
+        var=cat(2,['ref_data(G2data.',ref_vars{i,:},'==0,i-2)=nan;']);
         %----------------------------------------------------
         eval(var)
         clear var
-    elseif strcheck==1 & i>=53
+    elseif strcheck==1 & i>55
         %-----Modified by Ilaria Stendardo------------------
         %var=cat(2,['G2data(G2header{i,:}==0,i-3)=nan;']);
-        var=cat(2,['G2data(',G2header{i,:},'==0,i-3)=nan;']);
+        var=cat(2,['ref_data(G2data.',ref_vars{i,:},'==0,i-3)=nan;']);
         %----------------------------------------------------
+        eval(var)
+        clear var
+    elseif strcheck==1 & i==55
+        var=cat(2,['ref_data(G2data.',ref_vars{i,:},'==0,i-2)=nan;']);
+        eval(var)
+        clear var
+        var=cat(2,['ref_data(G2data.',ref_vars{i,:},'==0,i-4)=nan;']);
         eval(var)
         clear var
     end
     clear strcheck    
 end
-unique_cruises=unique(G2cruise,'stable');
+unique_cruises=unique(G2data.G2cruise,'stable');
 
-% the reference data file only contains the variables we do 2QC on in the toolbox
-ref_data=G2data(:,[1:18 25 27 32 37:3:49 54 58]); % save only those variables used by the 2nd QC toolbox
-
-I=[1:18 25 27 32 37:3:49 54 58];
-for i=1:length(I)
-ref_vars{i,:}=G2header{I(i),:};
-end
-ref_vars=ref_vars';
 
 % load expocodes
 fname=[cruise_folder filesep expocode_file_name];
 fid=fopen(fname);
 
-G2expocodes=textscan(fid,'%f%s','delimiter',',','headerlines',0); 
+G2expocodes=textscan(fid,'%f%s','delimiter',',','headerlines',1); 
 ref_UC=G2expocodes{1};
 ref_expocodes=G2expocodes{2};
-    
+
 % save as .mat file
 outputname=cat(2,[cruise_folder filesep outputfilename '.mat']); 
 save(outputname,'ref_data','ref_vars','unique_cruises','ref_UC','ref_expocodes');
 
 % create lookup table for subsampling the reference dataset when running Xovers
 outputname=cat(2,[cruise_folder filesep 'ReferencePositions_LookupTable.mat']);
-rlat=G2latitude; rlon=G2longitude; rcruise=G2cruise;
+rlat=G2data.G2latitude; rlon=G2data.G2longitude; rcruise=G2data.G2cruise;
 save(outputname,'rcruise','rlat','rlon');
 
 disp('.mat file created')
+
+
+
 
 
